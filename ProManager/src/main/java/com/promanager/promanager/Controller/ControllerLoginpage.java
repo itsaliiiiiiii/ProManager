47e8ea69338cbc4aedb5e175e76b2cbd97f54a19
ali harit <aliharit2@gmail.com>
2024-03-28 12:21:35 +0000

    nada

--- ProManager/src/main/java/com/promanager/promanager/Controller/ControllerLoginpage.java
+++ ProManager/src/main/java/com/promanager/promanager/Controller/ControllerLoginpage.java
@@ -1,10 +1,14 @@
 package com.promanager.promanager.Controller;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Reader;
 
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -30,39 +34,60 @@ public class ControllerLoginpage {
     private Text errorOutput;
 
     @FXML
-    Boolean verificationMail(ActionEvent event) {
+    public void initialize() {
+        try {
+            File file = new File(
+                    "/Users/its.aliiiiiiii/Desktop/Projects & Exercices/javaFX/ProManager/ProManager/src/main/java/com/promanager/promanager/Data/configuration.json");
+            Reader reader = new FileReader(file);
+            JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
+            if (jsonObject.has("email") && !(jsonObject.get("email").getAsString()).equals("emptyMail")) {
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(getClass().getResource("/com/promanager/promanager/View/main.fxml"));
+                Parent newRoot = loader.load();
+                Stage stage = (Stage) someComponent.getScene().getWindow();
+                stage.setScene(new Scene(newRoot, 1300, 800));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    void verificationMail(ActionEvent event) {
+        File file = new File(
+                "/Users/its.aliiiiiiii/Desktop/Projects & Exercices/javaFX/ProManager/ProManager/src/main/java/com/promanager/promanager/Data/configuration.json");
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String email = mail.getText();
         if (email.endsWith("@gmail.com")) {
             System.out.println("mail : " + email);
+            try (Reader reader = new FileReader(file)) {
+                JsonObject jsonObject = JsonParser.parseReader(reader).getAsJsonObject();
 
-            // --------
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            JsonObject jsonObject = new JsonObject();
-            jsonObject.addProperty("nnn", email);
-            String jsonString = gson.toJson(jsonObject);
+                if (jsonObject.has("email") && !(jsonObject.get("email").getAsString()).equals("emptyMail")) {
+                    try {
+                        FXMLLoader loader = new FXMLLoader();
+                        loader.setLocation(getClass().getResource("/com/promanager/promanager/View/main.fxml"));
+                        Parent newRoot = loader.load();
+                        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                        stage.setScene(new Scene(newRoot, 1300, 800));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    try {
 
-            try (FileWriter writer = new FileWriter("configuration.json",true)) {
-                writer.write(jsonString);
-                System.out.println("JSON object has been written to the file.");
-            } catch (IOException e) {
-                System.err.println("Error writing JSON object to file: " + e.getMessage());
+                        jsonObject.addProperty("email", email);
+                        FileWriter writer = new FileWriter(file);
+                        gson.toJson(jsonObject, writer);
+                        System.out.println("Email value has been updated in the JSON file.");
+                    } catch (IOException e) {
+
+                    }
+                }
             }
             // ------
-            // try {
-            // FXMLLoader loader = new FXMLLoader();
-            // loader.setLocation(getClass().getResource("/com/promanager/promanager/View/main.fxml"));
-            // Parent newRoot = loader.load();
-            // Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            // stage.setScene(new Scene(newRoot, 1300, 800));
-            // } catch (IOException e) {
-            // e.printStackTrace();
-            // }
-            return true;
         } else {
             errorOutput.setText("Adresse mail invalide.\nElle doit se terminer par  @gmail.com.");
             System.out.println("Adresse mail invalide. Elle doit se terminer par @gmail.com.");
-            return false;
         }
     }
 
