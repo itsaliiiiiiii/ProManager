a0efcbc153b341b0d2c470d64e8ff25643f3690e
Anas <anasszerhoun2019@gmail.com>
2024-03-29 22:23:47 +0000

    Create Project ProManager

---
+++ Folders/JavaProject/ProManager/src/Métier/Tache.java
@@ -0,0 +1,63 @@
+package Métier;
+import java.util.Date;
+
+public class Tache {
+    private Integer idTache;
+    private String categorieTache;
+    private String descriptionTache;
+    private Date dateDepartTache;
+    private Date dateFinTache;
+
+    public Tache() {
+    }
+
+    public Tache(Integer idTache, String categorieTache, String descriptionTache, Date dateDepartTache,
+            Date dateFinTache) {
+        this.idTache = idTache;
+        this.categorieTache = categorieTache;
+        this.descriptionTache = descriptionTache;
+        this.dateDepartTache = dateDepartTache;
+        this.dateFinTache = dateFinTache;
+    }
+
+    public Integer getIdTache() {
+        return idTache;
+    }
+
+    public void setIdTache(Integer idTache) {
+        this.idTache = idTache;
+    }
+
+    public String getCategorieTache() {
+        return categorieTache;
+    }
+
+    public void setCategorieTache(String categorieTache) {
+        this.categorieTache = categorieTache;
+    }
+
+    public String getDescriptionTache() {
+        return descriptionTache;
+    }
+
+    public void setDescriptionTache(String descriptionTache) {
+        this.descriptionTache = descriptionTache;
+    }
+
+    public Date getDateDepartTache() {
+        return dateDepartTache;
+    }
+
+    public void setDateDepartTache(Date dateDepartTache) {
+        this.dateDepartTache = dateDepartTache;
+    }
+
+    public Date getDateFinTache() {
+        return dateFinTache;
+    }
+
+    public void setDateFinTache(Date dateFinTache) {
+        this.dateFinTache = dateFinTache;
+    }
+
+}
---
+++ Folders/JavaProject/ProManager/src/Persistance/DaoSéance.java
@@ -0,0 +1,5 @@
+package Persistance;
+
+public class DaoSéance {
+    
+}
