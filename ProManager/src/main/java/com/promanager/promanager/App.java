2ef8991d2f854ca33857e3218c58214c75eb5505
Anas <anasszerhoun2019@gmail.com>
2024-04-02 23:59:47 +0000

    "TEST"

---
+++ ProManager/src/main/java/com/promanager/promanager/App.java
@@ -0,0 +1,42 @@
+package com.promanager.promanager;
+
+
+import org.bson.types.ObjectId;
+
+// import javafx.application.Application;
+// import javafx.fxml.FXMLLoader;
+// import javafx.scene.Parent;
+// import javafx.scene.Scene;
+// import javafx.stage.Stage;
+
+// import java.io.IOException;
+// import java.util.Objects;
+
+import com.promanager.promanager.Persistance.DAOprojet;
+
+public class App {
+    // @Override
+    // public void start(Stage stage) throws IOException {
+
+    // Parent root = FXMLLoader
+    // .load(Objects.requireNonNull(getClass().getResource("Presentation/View/LoginPage.fxml")));
+    // stage.setTitle("ProManager");
+    // stage.setScene(new Scene(root, 1300, 800));
+    // stage.setResizable(false);
+    // stage.show();
+    // }
+
+    public static void main(String[] args) {
+        // launch();
+        DAOprojet p = new DAOprojet();
+        // System.out.println(p.getAll());
+        System.out.println("-----");
+        // System.out.println(p.get(0));
+        // System.out.println(p.get(1));
+        
+        
+        p.Add("CATYGORIE", "TYPE", "First Project", new Date(), new Date());
+
+        //p.delete(p.get(new ObjectId("660c417b68aa02744a122aaf")).getIdProjet(),"Type");
+    }
+}
