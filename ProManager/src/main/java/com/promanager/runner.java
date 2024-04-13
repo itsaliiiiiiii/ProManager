3ee81a2cd178846e5e807088b3bc925237a56d8d
ali harit <aliharit2@gmail.com>
2024-04-13 18:47:39 +0000

    jar

---
+++ ProManager/src/main/java/com/promanager/runner.java
@@ -0,0 +1,11 @@
+package com.promanager;
+
+import com.promanager.promanager.App;
+
+public class runner {
+    public static void main(String[] args) {
+        System.out.println("Lets gooooooooooow !");
+        System.out.println("ProManager Application");
+        App.main(args);
+    }
+}
