aeccda7f50ffde7523a3b1d4644523ee1e214348
ali harit <aliharit2@gmail.com>
2024-04-14 11:25:16 +0100

    add icom image , but not working :>

--- ProManager/src/main/java/com/promanager/promanager/App.java
+++ ProManager/src/main/java/com/promanager/promanager/App.java
@@ -1,34 +1,35 @@
 package com.promanager.promanager;
 
-
 import com.promanager.promanager.Persistance.DAOconfiguration;
 import com.promanager.promanager.Presentation.View.LoginPage;
 import com.promanager.promanager.Presentation.View.ProjetView.ProjetsPage;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+
+
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        // Image logo = new Image(getClass().getResourceAsStream("Presentation/View/Logo/ProManager.png"));
+
+
         DAOconfiguration config = new DAOconfiguration();
         if (config.getMail().equals("empty")) {
             LoginPage loginPageView = new LoginPage(stage);
             Scene scene = new Scene(loginPageView, 1300, 800);
             stage.setScene(scene);
-            stage.setResizable(false);
-            stage.setTitle("ProManager");
-            stage.show();
         } else {
             ProjetsPage projetsPage = new ProjetsPage(stage, "tout", "tout");
             Scene projectsScene = new Scene(projetsPage, 1300, 800);
             stage.setScene(projectsScene);
-            stage.setMinWidth(1300);
-            stage.setMinHeight(800);
-            stage.setResizable(false);
-            stage.setTitle("ProManager");
-            stage.show();
         }
+        stage.setResizable(false);
+        stage.setTitle("ProManager");
+        stage.show();
     }
 
     public static void main(String[] args) {
