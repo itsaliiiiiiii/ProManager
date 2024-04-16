ac3588fcb5c5c8d6007448188d516c334b115775
ali harit <aliharit2@gmail.com>
2024-04-16 16:21:49 +0100

    push

--- ProManager/src/main/java/com/promanager/runner.java
+++ ProManager/src/main/java/com/promanager/runner.java
@@ -4,7 +4,6 @@ import com.promanager.promanager.ProManager;
 
 public class runner {
     public static void main(String[] args) {
-        System.out.println("Lets gooooooooooow !");
         System.out.println("ProManager Application");
         ProManager.main(args);
     }
